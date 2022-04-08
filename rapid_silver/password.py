8c53171b6c51d4c5355aac632a0d3d4a5fd89d1c
KristianColville1 <kristiancolville1@gmail.com>
2022-04-08 19:26:58 +0100

    Wire up APIs and confirm access to google sheet

--- rapid_silver/password.py
+++ rapid_silver/password.py
@@ -1,18 +1,9 @@
 """Hold the password class for Rapid Silver"""
-import gspread
-from google.oauth2.service_account import Credentials
 
-
-class Password(User):
+class Password():
     """
     Creates a password object for the user.
     """
-    CREDS = Credentials.from_service_account_file('creds.json')
-    SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
     
     def __init__(self, password):
         self.password = password()
--- rapid_silver/user.py
+++ rapid_silver/user.py
@@ -3,6 +3,8 @@
 class User():
     """Creates a user object for the current user of the terminal"""
     
-    def __init__(self):
-        
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
     
