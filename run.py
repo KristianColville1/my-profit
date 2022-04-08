a23190f4b3cc0f52b06675c0b7608a5fb31e786c
KristianColville1 <kristiancolville1@gmail.com>
2022-04-08 11:39:38 +0100

    Add log in screen portal for users

--- run.py
+++ run.py
@@ -17,13 +17,44 @@ def welcome_msg():
         return "Welcome to MyProf"
     return welcome
 
+def login_screen(color):
+    """
+    When called it displays the log in screen to the terminal.
+    """
+    try:
+        clear_console()
+        file = open('login.txt')
+        login_msg = file.read()
+        print('\n\n' + color.p_cyan(login_msg))
+    except IOError:
+        # in event reading log in message fails
+        print('Log in screen')
+
+    print(color.p_yellow("\n\t\t\tHit 's' + Enter to login"))
+    print(color.p_yellow("\t\t\tHit 'm' + Enter to create new account"))
+    print(color.p_yellow("\t\t\tHit 'd' + Enter for instructions"))
+
+    try:
+        result = input(color.p_green('\n\t\t\tHere: '))
+        if result not in ('s', 'm', 'd'):
+            print(color.p_red('\t\t\tInvalid input, enter s, m or d'))
+            time.sleep(2.5)
+            login_screen(color)
+    except ValueError:
+        login_screen(color)
+    return result
+
 
 def main():
     """
     Runs the program.
     """
-    text = c_print.ColorPrint()
-    print(text.print_red(welcome_msg()))
-    input(text.print_yellow('\n\t\t\t< Press enter to continue to log in >'))
+    color = c_print.ColorPrint()
+    print(color.p_red(welcome_msg()))
+    input(color.p_yellow('\n\t\t\t< Press enter to continue to log in >'))
+    result = login_screen(color)
+    clear_console()
+    print(result)
 
+SIGNED_IN = False
 main()
