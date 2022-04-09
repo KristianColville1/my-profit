eb20decf636ed23baa75d11d22b9c8493e922fa7
KristianColville1 <kristiancolville1@gmail.com>
2022-04-09 19:05:31 +0100

    Add methods for hash and money symbol loading animations

--- rapid_silver/art.py
+++ rapid_silver/art.py
@@ -18,8 +18,37 @@ class TextArt(ColorPrint):
         Used to add more visually pleasing terminal. Acts as a time delay
         between getting and setting data.
         """
+
         text = 'Loading'
         for _ in range(25):
             text += self.p_cyan(self.dot_sym)
+            print(f'{text}\r', end='')
+            time.sleep(0.3)
+
+
+    def hash_loading(self):
+        """
+        Uses carriage return to make a basic animation for hash symbols.
+        Used to add more visually pleasing terminal. Acts as a time delay
+        between getting and setting data.
+        """
+
+        text = 'Loading'
+        for _ in range(25):
+            text += self.p_yellow(self.hash_sym)
+            print(f'{text}\r', end='')
+            time.sleep(0.3)
+
+
+    def money_loading(self):
+        """
+        Uses carriage return to make a basic animation for money symbols.
+        Used to add more visually pleasing terminal. Acts as a time delay
+        between getting and setting data.
+        """
+
+        text = 'Loading'
+        for _ in range(25):
+            text += self.p_green(self.money_sym)
             print(f'{text}\r', end='')
             time.sleep(0.3)
