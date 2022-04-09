d09defbd35529b3d8f9042c79fe400e5e99ed524
KristianColville1 <kristiancolville1@gmail.com>
2022-04-09 19:02:30 +0100

    Add text art class for simple loading animations

---
+++ rapid_silver/art.py
@@ -0,0 +1,25 @@
+"""Holds text art class for Rapid Silver"""
+import time
+from rapid_silver.c_print import ColorPrint
+
+class TextArt(ColorPrint):
+    """
+    Creates text art objects for the terminal.
+    Provides simple text animations for loading and updating content etc.
+    """
+
+    hash_sym = '#'
+    dot_sym = '.'
+    money_sym = '$'
+
+    def dot_loading(self):
+        """
+        Uses carriage return to make a basic animation for dot symbols.
+        Used to add more visually pleasing terminal. Acts as a time delay
+        between getting and setting data.
+        """
+        text = 'Loading'
+        for _ in range(25):
+            text += self.p_cyan(self.dot_sym)
+            print(f'{text}\r', end='')
+            time.sleep(0.3)
