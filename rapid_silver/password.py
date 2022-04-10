5cb271621ba0e2731ff2b69657b3c67dc169baee
KristianColville1 <kristiancolville1@gmail.com>
2022-04-10 10:20:39 +0100

    Refactor class Password to PasswordManager

--- rapid_silver/password.py
+++ rapid_silver/password.py
@@ -1,28 +1,12 @@
 """Hold the password class for Rapid Silver"""
 import gspread
 from google.oauth2.service_account import Credentials
+from cryptography.fernet import Fernet
 
-class Password(object):
+
+class PasswordManager():
     """
-    Creates a password object for the user.
+    Handles the password information for the user of
+    Rapid Silver.
     """
     
-    SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-    CREDS = Credentials.from_service_account_file('creds.json')
-    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-    SHEET = GSPREAD_CLIENT.open('rapid_silver')
-
-    user_names = SHEET.worksheet('rapid_silver')
-    data = user_names.get_all_values()
-    def __init__(self, password):
-        self.password = password
-
-
-    def get_password(self):
-        """"""
