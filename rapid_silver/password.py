99610ac7f33c0ae28a0a9cd12f941d01a9e81ee1
KristianColville1 <kristiancolville1@gmail.com>
2022-04-11 19:51:14 +0100

    Set up method structure for passwordmanager class

--- rapid_silver/password.py
+++ rapid_silver/password.py
@@ -1,12 +1,28 @@
 """Hold the password class for Rapid Silver"""
-import gspread
-from google.oauth2.service_account import Credentials
+from getpass import getpass
 from cryptography.fernet import Fernet
 
 
-class PasswordManager():
+class PasswordManager:
     """
     Handles the password information for the user of
     Rapid Silver.
     """
-    
+    def __init__(self, account_type):
+        if account_type == 'new':
+            self.set_user_password()
+            self.log_in_user()
+        elif account_type == 'old':
+            self.get_login_details()
+            self.log_in_user()
+
+    def set_user_password(self):
+        return ''
+    def log_in_user(self):
+        return ''
+    def get_login_details(self):
+        return ''
+    # TODO: Create salt, pepper and hashing
+    # TODO: Store inside a database
+    # TODO: block user password from being stored anywhere in plain text
+    # TODO: get all code and structure it properly
