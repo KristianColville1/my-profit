86efe19bc3bff71c72696b104fad82ea84559888
KristianColville1 <kristiancolville1@gmail.com>
2022-04-12 10:29:08 +0100

    Refactor username generator and begin building password manager

--- rapid_silver/password.py
+++ rapid_silver/password.py
@@ -1,28 +1,69 @@
 """Hold the password class for Rapid Silver"""
+import os
+from pymongo import MongoClient
 from getpass import getpass
 from cryptography.fernet import Fernet
+from console import clear_console
 
 
-class PasswordManager:
+class PasswordManager():
     """
     Handles the password information for the user of
     Rapid Silver.
     """
-    def __init__(self, account_type):
-        if account_type == 'new':
-            self.set_user_password()
-            self.log_in_user()
-        elif account_type == 'old':
-            self.get_login_details()
-            self.log_in_user()
+    character_dict = {
+        'a': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        'b': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+              'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+              'W', 'X', 'Y', 'Z']
+    }
 
-    def set_user_password(self):
+    character_keys = ['a', 'b']
+
+    profile_dict = {
+        "USERNAME": "",
+        "PASSWORD": ""
+    }
+
+    def __init__(self, account_type):
+        # temporary catch claus for local development and live terminal development
+        try:
+            self.mongopass = os.environ.get('MONGOPASSWORD')
+        except NameError:
+            file = open('mongodb.txt', encoding='utf8')
+            self._mongopass = file.read()
+            file.close()
+        if account_type == 'new':
+            self._set_username()
+            self._set_user_password()
+            self._log_in_user()
+        elif account_type == 'old':
+            self._get_login_details()
+            self._log_in_user()
+
+    def _set_username(self):
+        
+        return None
+    def _set_user_password(self):
         return ''
-    def log_in_user(self):
+    def _log_in_user(self):
         return ''
-    def get_login_details(self):
+    def _get_login_details(self):
         return ''
     # TODO: Create salt, pepper and hashing
     # TODO: Store inside a database
     # TODO: block user password from being stored anywhere in plain text
     # TODO: get all code and structure it properly
+
+
+cluster = MongoClient(f"mongodb+srv://rapid_silver_educate:{password}@rapidsilver.h5hbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster['RapidSilver']
+collection = db['users']
+
+user_name = input('Enter your username: ')
+password = input('Enter your password: ')
+
+
+# leave id blank as it needs unique identifiers on each post and it current does not matter
+post = { user_name:password, "data":"company"}
+collection.insert_one(post)
