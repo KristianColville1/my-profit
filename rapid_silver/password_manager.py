ee6358f5b5e4b31cdf7bd735892c1fed8fd3f677
KristianColville1 <kristiancolville1@gmail.com>
2022-04-12 16:15:31 +0100

    Update password_manager after PEP8 testing

--- rapid_silver/password_manager.py
+++ rapid_silver/password_manager.py
@@ -8,9 +8,10 @@ from pymongo import MongoClient
 from console import clear_console
 from rapid_silver.text_art import TextArt
 
-
 # instance created
 color = TextArt()
+
+
 class PasswordManager():
     """
     Handles the password information for the user of
@@ -34,7 +35,8 @@ class PasswordManager():
 
     def __init__(self, account_type):
 
-        # temporary catch claus for local development and live terminal development
+        # temporary catch claus for local development and
+        # live terminal development
         try:
             self._mongopass = os.environ.get('MONGOPASSWORD')
         except NameError:
@@ -56,17 +58,20 @@ class PasswordManager():
             self._log_in_user()
 
         # line long but link wont work without it formatted like this
-        self.cluster = MongoClient(f"mongodb+srv://rapid_silver_educate:{self._mongopass}@rapidsilver.h5hbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority""")
+        line_a = f'mongodb+srv://rapid_silver_educate:{self._mongopass}'
+        line_b = '@rapidsilver.h5hbo.mongodb.net/myFirstDatabase?retryWrites'
+        line_c = '=true&w=majority'
+        self.cluster = MongoClient(line_a + line_b + line_c)
         self.database = self.cluster['RapidSilver']
         self.collection = self.database['users']
-
 
     def _set_username(self):
         clear_console()
         for _ in range(8):
             print('\n')
-
-        print('Would you like to set a username or have a username generated? y/n')
+        text = 'Would you like to set a username'
+        text += ' or have a username generated? y/n'
+        print(f'{text}')
         result = input(color.cyan_fore('Enter here: '))
 
         if result in ('Y', 'y'):
@@ -77,16 +82,22 @@ class PasswordManager():
             clear_console()
             try:
 
-                advice = color.yellow_fore('Please include (A-Z & a-z) and at least 1 number')
+                advice = color.yellow_fore('Please include (A-Z & a-z)')
+                advice += color.cyan_fore('and at least 1 number')
                 print(advice)
                 username_input = input('Please enter a username: ')
 
                 if len(username_input) < 8:
-                    raise ValueError('Username must be at least 8 characters')
+                    raise ValueError(
+                        'Username must be at least 8 characters'
+                        )
                 if len(username_input) > 40:
-                    raise ValueError('Username must be less than 40 characters')
-
-                validation = self._check_characters_valid_in('username', username_input)
+                    raise ValueError(
+                        'Username must be less than 40 characters'
+                        )
+                user = username_input
+                input_type = 'username'
+                validation = self._check_characters_valid_in(input_type, user)
                 if validation is False:
                     raise ValueError('You must include at least one number')
                 # TODO: check database for conflicts in username here also
@@ -127,9 +138,13 @@ class PasswordManager():
         print('You need a password of at least 10 characters')
         print(color.red_fore('It must contain at least 1 number'))
         print(color.red_fore('It must also contain at least 1 capital letter'))
-        print(color.red_fore('It must contain at least 1 of the special symbols below'))
+        print(color.red_fore(
+            'It must contain at least 1 of the special symbols below'
+            ))
         print(f'\n{self.special_chars}')
-        print(color.yellow_fore('\n\t**Please note for your protection password input is hidden**'))
+        print(color.yellow_fore(
+            '\n\t**Please note for your protection password input is hidden**'
+            ))
         password_one = getpass('\n\nEnter your password here: ')
         password_two = getpass('Enter your password again to confirm: ')
 
@@ -139,26 +154,30 @@ class PasswordManager():
             print(color.red_fore('PASSWORDS NOT MATCHING, try again.'))
             time.sleep(2)
             self._set_user_password()
-        checker = True
+
+        checker = True  # checker is true until proven invalid
         for char in password:
             if char not in self.alphabet and char not in self.special_chars:
                 if char not in self.character_dict['a']:
                     checker = False
                     break
+
         if checker is False:
             print(color.red_fore('\n INVALID password chosen, try again'))
             time.sleep(2.5)
             self._set_user_password()  # used recursion until valid password
+
         if len(password) < 10:
             print('Password too short, please try again')
 
-        print(password)
-        return ''
+        return password
 
     def _log_in_user(self):
         return ''
+
     def _get_login_details(self):
         return ''
+
     def _check_characters_valid_in(self, to_check, input_type):
         result = True
         if to_check == 'username':
@@ -178,12 +197,5 @@ class PasswordManager():
     # TODO: block user password from being stored anywhere in plain text
     # TODO: get all code and structure it properly
 
-
-
-
-
-
-
-# leave id blank as it needs unique identifiers on each post and it current does not matter
 # post = { user_name:password, "data":"company"}
 # collection.insert_one(post)
