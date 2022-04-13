e1af13725c979af04b5f21d435c6de06ee721c58
KristianColville1 <kristiancolville1@gmail.com>
2022-04-13 16:15:54 +0100

    Refactor username in password manager for global scope

--- rapid_silver/password_manager.py
+++ rapid_silver/password_manager.py
@@ -85,7 +85,7 @@ class PasswordManager():
                     raise ValueError('You must include at least one number')
                 # TODO: check database for conflicts in username here also
 
-                self._username = username_input
+                self.username = username_input
             except ValueError as error:
                 error = color.red_fore(error)
                 print(error)
@@ -97,7 +97,7 @@ class PasswordManager():
             time.sleep(1.5)
             self._set_username()
 
-        return self._username
+        return self.username
 
     def _generate_username(self):
         """
