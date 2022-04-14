4233b1e20a40cce37f83df68b7158ef5d2d2ae02
KristianColville1 <kristiancolville1@gmail.com>
2022-04-14 21:28:17 +0100

    Link up userclass and accessing and setting up user profile and test

--- rapid_silver/data_manager.py
+++ rapid_silver/data_manager.py
@@ -2,10 +2,15 @@
 Data manager hold the DataManager Class for Rapid Silver.
 Manages user data for a given user profile.
 """
-import bcrypt
+import os
+import time
 from pymongo import MongoClient
 from console import clear_console
 from rapid_silver.text_art import TextArt
+from rapid_silver.user import User
+
+# instances created
+color = TextArt()
 
 
 class DataManager():
@@ -14,11 +19,37 @@ class DataManager():
     Stores the users data entered. Updates the users data. It can only be used
     with the PasswordManagers credentials for the logged in user.
     """
+
+    monogo_link = os.environ.get('MONGOLINK')
+    _cluster = MongoClient(f"{monogo_link}")
+    _database = _cluster['RapidSilver']
+    _user_details_collection = _database['users-details']
+
     def __init__(self, user_id):
-        self.user = user_id
-        self.data_profile = None
-    
-    
-    def _check_for_user_profile(self, profile):
+        self.username = user_id
+
+        if self.check_details_database(self.username) is None:
+            self.user = User()
+            self.add_details_to_database(self.username, self.user.user_details)
+
+    def check_details_database(self, username):
         """
+        Checks to see if name is available to the user.
         """
+        result = self._user_details_collection.find_one({"_id": username})
+        time.sleep(1)
+        return result
+
+    def add_details_to_database(self, user_id, dict_of_user_details):
+        """
+        Adds details from user class to a dict to send to the details
+        collection in MongoDB.
+        """
+        details_to_send = {}
+        details_to_send["_id"] = str(user_id)
+
+        for key, value in dict_of_user_details.items():
+            details_to_send[key] = value
+
+        post = details_to_send
+        self._user_details_collection.insert_one(post)
