6c55817be6825235fb0529b3d5ac244d08c89da5
KristianColville1 <kristiancolville1@gmail.com>
2022-04-15 20:43:28 +0100

    Add inventory route using todo list code as template

--- rapid_silver/data_manager.py
+++ rapid_silver/data_manager.py
@@ -41,12 +41,8 @@ class DataManager():
                 self.print_welcome_back()
         elif the_route == 'to_do':
             self.open_to_do_list()
-        elif the_route == 'products':
-            pass
-        elif the_route == 'employee':
-            pass
         elif the_route == 'inventory':
-            pass
+            self.open_inventory()
 
     def check_user_details(self):
         """
@@ -134,7 +130,7 @@ class DataManager():
         for the user.
         """
         clear_console()
-        to_do_list = self._to_do_collection.find_one({"_id": self.username})  
+        to_do_list = self._to_do_collection.find_one({"_id": self.username})
         if to_do_list is None:
             # creates a to do list if none exists
             self._to_do_collection.insert_one({'_id': self.username})
@@ -254,3 +250,132 @@ class DataManager():
                 if result in ('n', 'N'):
                     break
                 self.clear_update_to_do_list()  # prints again and asks
+
+    def open_inventory(self):
+        """
+        A user of Rapid Silver can create and update inventory.
+        Checks for existing inventory and if theres none there creates one
+        for the user.
+        """
+        clear_console()
+        inventory = self._inventory_collection.find_one({"_id": self.username})
+        if inventory is None:
+            # creates a inventory if none exists
+            self._inventory_collection.insert_one({'_id': self.username})
+            print(
+                color.red_fore(
+                    'There are no inventories matching our records for you'))
+            print(
+                color.yellow_fore('\n Lets create one now.')
+            )
+            while True:
+                try:
+                    print(color.red_fore(
+                        '\nTo leave just hit [ n ] + Enter\n'))
+                    result = input('Enter here: ')
+                    if result in ('n', 'N'):
+                        break
+                    key = str(input(
+                        color.purple_fore(
+                            "\n\nEnter a name for the item here: ")))
+                    value = str(input(
+                        color.yellow_fore(
+                            '\nEnter the amount here :')))
+
+                    to_move_on = str(input(
+                        color.yellow_fore(
+                            'To create another item hit [ y ] + Enter : ')))
+
+                    if len(key) > 0 and len(value) > 0:
+                        self._inventory_collection.update_one(
+                            {"_id": self.username}, {"$set": {key: value}})
+                    if to_move_on in ('Y', 'y'):
+                        pass
+                    else:
+                        break
+                except ValueError:
+                    pass
+                except TypeError:
+                    pass
+        # after inventory is either found or created, print it to the terminal
+        # calls to get the document again so both routes are
+        # accounted for and joined back here
+        inventory = self._inventory_collection.find_one({"_id": self.username})
+        table = Table()
+        table.add_column('Item', style='blue')
+        table.add_column('Task', style='yellow')
+
+        if inventory is not None:
+            for key, value in inventory.items():
+                # hides the id and username
+                if key != '_id':
+                    table.add_row(str(key), str(value))
+
+            console = Console()
+            console.print(table)
+
+        print(color.red_fore(
+            "\nWould you like to update your to do list?"
+        ))
+        print(
+            "Hit [ y ] + enter to update your to do list: "
+        )
+
+        result = input(color.yellow_fore(
+            'Enter your choice here, hit enter to skip: '
+        ))
+        color.dot_loading('Updating to do list now')
+        if result in ('y', 'Y'):
+            self.clear_update_inventory()
+        else:
+            pass
+
+    def print_inventory(self):
+        """
+        Prints the inventory to the terminal to view.
+        """
+        inventory = self._inventory_collection.find_one({"_id": self.username})
+        table = Table()
+        table.add_column('Item', style='blue')
+        table.add_column('Amount', style='yellow')
+
+        for key, value in inventory.items():
+            # hides the id and username
+            if key != '_id':
+                table.add_row(str(key), str(value))
+
+        console = Console()
+        console.print(table)
+
+    def clear_update_inventory(self):
+        """
+        Clears or makes amendments to a inventory list for a user.
+        """
+        clear_console()
+        self.print_inventory()
+
+        print('\n\n\nIf you would like to empty your inventory')
+        print("Please enter 'empty inventory' below")
+        print('Hit Enter otherwise ')
+        result = input(color.cyan_fore('Enter here please'))
+        if result == 'empty inventory':
+            self._inventory_collection.delete_one({"_id": self.username})
+        else:
+            while True:
+                clear_console()
+                self.print_inventory()
+                inventory_name = input(
+                    color.yellow_fore(
+                        'Enter the name of item you wish to update: '))
+                the_amount = input(
+                    color.yellow_fore('Now begin updating amounts here: '))
+
+                self._inventory_collection.update_one(
+                    {"_id": self.username}, {
+                        "$set": {inventory_name: the_amount}})
+                print('\n\nTo exit hit [ n ] + Enter ')
+                print('\nEnter [ y ] to update another inventory item')
+                result = input('Enter here please: ')
+                if result in ('n', 'N'):
+                    break
+                self.clear_update_inventory()  # prints again and asks
--- run.py
+++ run.py
@@ -254,7 +254,7 @@ def set_up_profile(validated_user):
     """
     clear_console()
     print('\n\n\n\n\nLets check to see if you have a profile first.')
-    LOADING.hash_loading('Checking for user profile')
+    LOADING.star_loading('Checking for user profile')
     DataManager(validated_user.username, 'profile')  # opens the data manager
     time.sleep(2)
     open_selection_menu(validated_user)
@@ -275,6 +275,10 @@ def store_update_stock_for(validated_user):
     """
     Sets up a logged in users profile and returns to the selection menu.
     """
+    clear_console()
+    LOADING.money_loading('Checking your inventory now')
+    DataManager(validated_user.username, 'inventory')  # opens the data manager
+    time.sleep(2)
     open_selection_menu(validated_user)
 
 
